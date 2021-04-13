c3b216be452563f31ec169a0cbc66fe8335d61e3
asladej1 <63958414+asladej1@users.noreply.github.com>
2021-04-12 20:37:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 # Import your Game class
 from game import Game
+from phrase import Phrase
 
 # Create your Dunder Main statement.
 if __name__ == '__main__':
-    go = Game()
-    go.start_game()
-# Inside Dunder Main:
-## Create an instance of your Game class
-## Start your game by calling the instance method that starts the game loop
+    
+        
+    game = Game()
+    print(game.active_phrase.phrase)
+    game.start()
+    
