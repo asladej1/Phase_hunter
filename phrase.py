7144497d7b3658135024d25080c3c59e26fba5ba
asladej1 <63958414+asladej1@users.noreply.github.com>
2021-04-12 20:36:20 -0500

    Update phrase.py

--- phrase.py
+++ phrase.py
@@ -1,45 +1,35 @@
-class Phrase():
+# Create your Phrase class logic here.
+
+
+
+class Phrase:
     
+    def __init__(self, phrase):
+        self.phrase = phrase.lower()
     
-    
-    
-    def __init__(self,phrase):
-        self.phrase = phrase
+    def display(self, guesses):
+        display = []
         
-        
-    def display(self, phrase):
-        display_phrase = []
-        self.phrase = phrase
-        self.guess = guess
-      
-       
-        #Can't get this to run
-        #while check_complete() == False:
         for letter in self.phrase:
-#            if self.guess in self.phrase:
-#                display_phrase.append(letter)
-            if letter == ' ':
-                display_phrase.append(' ')       
+            if letter in guesses:
+                display.append(letter)
+            elif letter == ' ':
+                display.append(' ')       
             else:
-                display_phrase.append('_')
-        print(' '.join(display_phrase))
-        
-         
-    def check_letter(self,guess):
+                display.append('_')
+        print(' '.join(display))       
+      
+    def check_guess(self, guess):
         if guess in self.phrase:
             return True
-        else:
-            return False
-        
-        
-    def check_complete(self):
-        if self.display() == self.phrase:
-            return True
+
         else:
             return False
     
-    
-    
-#go = Phrase()
-#go.display("j")
-#go.check_letter("j")
+    def check_complete(self, guesses):
+        for letter in self.phrase:
+            if letter not in guesses:
+                return False
+            else:
+                return True
+        
