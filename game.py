fa2d93cc99d147b8912b4b9827b5afad583afd80
asladej1 <63958414+asladej1@users.noreply.github.com>
2021-04-14 19:24:31 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,61 +1,36 @@
-# Create your Game class logic in here.
-from phrase import Phrase
-import random
+# Create your Phrase class logic here.
 
 
 
-class Game:
+class Phrase:
     
-    def __init__(self):
-        self.missed = 0
-        self.phrases = [Phrase('Is the juice worth the squeeze'), Phrase('Green Lights'), Phrase('An Arm and a Leg'), Phrase('Dime a Dozen'), Phrase('Down to the Wire')]
-        self.active_phrase = self.get_random_phrase()
-        self.guesses = [" "]
-        
-    def welcome(self):
-        GREETING = "==========================\n Welcome to Phrase Hunter\n=========================="
-        print(GREETING.upper())
-        rules = print("\nRules: \n******Try and guesses the phrase! Select a letter on a time, if you can't solve it before your 5 guess up you lose. Good luck!.******\n\n")    
+    def __init__(self, phrase):
+        self.phrase = phrase.lower()
     
-    def get_random_phrase(self):
-        sel_phrase = random.choice(self.phrases)
-        return sel_phrase
-    
-    def start(self):
-        self.welcome()
-        while self.missed < 5 and self.active_phrase.check_complete(self.guesses) == False:
-          print(f'Number missed: {self.missed}')
-          self.active_phrase.display(self.guesses)
-          user_guess = self.get_guess()
-          self.guesses.append(user_guess)
-          if not self.active_phrase.check_guess(user_guess):
-            self.missed += 1
-          self.active_phrase.check_complete(self.guesses)
-        self.game_over()
+    def display(self, guesses):
+        display = []
         
-    def game_over(self):
-      if self.missed == 5:
-        print("Sorry, Game Over...")
-      else:
-        print("You guessed it right! Congratualations!")
-        
-        
-    def get_guess(self):
-        try:
-          self.guess = input('Please enter letter: ')
-          
+        for letter in self.phrase:
+            if letter in guesses:
+                display.append(letter)
+            elif letter == ' ':
+                display.append(' ')       
+            else:
+                display.append('_')
+        print(' '.join(display))       
       
-          
-          if len(self.guess) > 1:
-            print('1')
-          
-          else:
-            raise ValueError
-          
-          #else:
-          #return self.guess
-        
-        except ValueError as err:
-          print('\nInvalid input. One letter at a time.')
-        
+    def check_guess(self, guess):
+        if str(guess) in self.phrase:
+            return True
+
+        else:
+            return False
     
+    def check_complete(self, guesses):
+        for letter in self.phrase:
+            if str(letter) not in guesses:
+                return False
+            else:
+                return True
+        
+      
